32091897aa676f893c6223b56e823ebf0ab25f61
Oscar.Ornelas <oscarornelas98123@gmail.com>
2020-04-01 15:02:06 -0600

    Updates

---
+++ precache-manifest.31b758e7afaf6ca16a534cc8cf59ffc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23cf8864dddb9c755269ed94e9515b72",
+    "url": "/classical-junkie/index.html"
+  },
+  {
+    "revision": "e9d4672f6c681fc95af8",
+    "url": "/classical-junkie/static/css/main.f244ba82.chunk.css"
+  },
+  {
+    "revision": "322fabf751373434bda8",
+    "url": "/classical-junkie/static/js/2.7fdfb9ba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/classical-junkie/static/js/2.7fdfb9ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9d4672f6c681fc95af8",
+    "url": "/classical-junkie/static/js/main.b895ec30.chunk.js"
+  },
+  {
+    "revision": "b3fe7cf322924c511413",
+    "url": "/classical-junkie/static/js/runtime-main.d70476b4.js"
+  },
+  {
+    "revision": "3af6fdb3dc9a77b00dffcc35463e5f3b",
+    "url": "/classical-junkie/static/media/home_background.3af6fdb3.PNG"
+  }
+]);
